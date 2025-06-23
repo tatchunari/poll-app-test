afe098fa227df25a82931843eb58c7dc4d91e641
Orathai Saengsoemsap <mayomsaki@gmail.com>
2025-06-23 21:14:12 +0700

    Adjust font size and Question card size

--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -2,20 +2,20 @@ const QuestionCard = ({ question, onSelect }) => {
   return (
       <div
         onClick={() => onSelect(question)}
-        className="flex flex-col items-center justify-center cursor-pointer overflow-hidden rounded-xl bg-white border-blue-400 border-2 hover:bg-amber-100  hover:border-amber-400 transition-transform duration-300 ease-in-out transform hover:-translate-y-2 hover:shadow-lg"
+        className="flex flex-col items-center justify-center cursor-pointer overflow-hidden rounded-xl bg-white border-blue-400 border-2 h-[288px] w-full max-w-[318px] hover:bg-amber-100  hover:border-amber-400 transition-transform duration-300 ease-in-out transform hover:-translate-y-2 hover:shadow-lg"
       >
         {/* Image */}
-          <div className="bg-white w-[90%] h-40 mb-3 overflow-hidden rounded-lg mt-3">
+          <div className="bg-white mb-6 w-[90%] overflow-hidden rounded-lg mt-3">
             <img
               src={question.img}
               alt={question.text}
-              className="w-full h-full object-cover"
+              className="object-contain w-full max-h-full"
             />
           </div>
 
        {/* Question Text */}
-       <div className=" text-center w-[90%]">
-        <h3 className="font-secondary text-black font-bold text-[12px] pb-4">
+       <div className="text-center w-[90%]">
+        <h3 className="font-secondary text-black font-bold text-[16px] pb-6">
           {question.text}
         </h3>
       </div>
--- src/pages/QuestionPage.jsx
+++ src/pages/QuestionPage.jsx
@@ -12,11 +12,11 @@ const QuestionPage = ({ onPick }) => {
       {/* Header */} 
       <div className='flex flex-col text-center w-full max-w-[90vw] sm:max-w-[500px] mx-auto'>
       <h2 className='font-primary text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-black'>SELECT YOUR TOPIC</h2>
-      <p className='font-primary text-[10px] sm:text-[12px] md:text-sm lg:text-base text-black my-6'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur consequuntur fugiat nostrum id amet consectetur eveniet a quas quo quibusdam perferendis nam dignissimos molestiae culpa eligendi incidunt quos, unde aut!</p>
+      <p className='font-secondary text-[16px] text-black my-6'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur consequuntur fugiat nostrum id amet consectetur eveniet a quas quo quibusdam perferendis nam dignissimos molestiae culpa eligendi incidunt quos, unde aut!</p>
       </div>
 
       {/* Question Card */}
-      <div className='grid grid-cols-1 md:grid-cols-3 max-w-[1024px] gap-4 items-center'>
+      <div className='grid grid-cols-1 md:grid-cols-3 max-w-[1024px] gap-4 items-center w-full mx-auto place-items-center'>
         {questions.map((q) => (
         <QuestionCard key={q.id} question={q} onSelect={onPick} />
       ))}
